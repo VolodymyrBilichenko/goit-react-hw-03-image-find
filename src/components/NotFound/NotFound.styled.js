00456e67c5a9f0494f22169b037aa21f471e0c38
Volodymyr <61534586+VolodymyrBilichenko@users.noreply.github.com>
2023-08-24 16:34:22 +0300

    update
    create massege nothing found

---
+++ src/components/NotFound/NotFound.styled.js
@@ -0,0 +1,10 @@
+import { styled } from "styled-components";
+
+export const NotFoundStyle = styled.li`
+    text-align: center;
+
+    .not__found{
+        color: #000;
+        font-size: 45px;
+    }
+`
